aa248bd447b9152ad3fd0a0a9b859e837b8c119f
AbraaoM <abraaol.moreira@outlook.com>
2019-09-15 01:45:51 -0300

    algumas correções

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,66 +1,68 @@
 import React from 'react';
 import { View, Image, StyleSheet, KeyboardAvoidingView, Text, TextInput, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import axios from axios;
+import axios from 'axios';
 
-export default class User extends Component {
+
+export default class Login extends React.Component {
   state = {
     email: '',
     password: '',
   };
-}
-export default login = async() => {
-    url = 'localhost:3000/users?email='+ this.email + '&password='+ this.password;
+
+  signin = async() => {
+    url = 'https://10.0.2.2:3000/users?email=' + this.email + '&password=' + this.password
     axios.get(url)
-    .then((res) => {
-      console.log(res);
-      goToHome;
-  })
-  .catch(console.error());
+    .then((res) =>{
+      console.log(res),
+      this.goToHome
+    })
+    .catch(console.error())
+  }
   
-}
-   goToSignUp = () => {
+  goToSignUp = () => {
     Actions.signup()
   }
-  const goToHome = () => {
+  goToHome = () => {
     Actions.home()
   }
   render(){
-  return (
-    <KeyboardAvoidingView style={styles.container}>
-      <View style={styles.loginContainer}>
-        <Image resizeMode="contain" style={styles.logo} source={require('../../components/images/itrade-logo.png')} />
-      </View>
+    return (
+      <KeyboardAvoidingView style={styles.container}>
+        <View style={styles.loginContainer}>
+          <Image resizeMode="contain" style={styles.logo} source={require('../../components/images/itrade-logo.png')} />
+        </View>
 
-      <View style={styles.formContainer}>
-        <TextInput style = {styles.input}
-          autoCapitalize="none"
-          onSubmitEditing={() => this.passwordInput.focus()}
-          onChangeText={(email) => this.setState({email})}
-          autoCorrect={false}
-          keyboardType='email-address'
-          returnKeyType="next"
-          placeholder='Email'
-          placeholderTextColor='#CD7F32'/>
+        <View style={styles.formContainer}>
+          <TextInput style = {styles.input}
+            autoCapitalize="none"
+            onSubmitEditing={() => this.passwordInput.focus()}
+            onChangeText={(email) => this.setState({email})}
+            autoCorrect={false}
+            keyboardType='email-address'
+            returnKeyType="next"
+            placeholder='Email'
+            placeholderTextColor='#CD7F32'/>
 
-        <TextInput style = {styles.input}
-          returnKeyType="go"
-          ref={(input)=> this.passwordInput = input}
-          onChangeText={(password) => this.setState({password})}
-          placeholder='Senha'
-          placeholderTextColor='#CD7F32'
-          secureTextEntry/>
+          <TextInput style = {styles.input}
+            returnKeyType="go"
+            ref={(input)=> this.passwordInput = input}
+            onChangeText={(password) => this.setState({password})}
+            placeholder='Senha'
+            placeholderTextColor='#CD7F32'
+            secureTextEntry/>
 
-        <TouchableOpacity style={styles.buttonContainer} onPress= {this.Login} >
-          <Text style={styles.buttonText}>LOGIN</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonContainer} onPress = {this.signin} >
+            <Text style={styles.buttonText}>LOGIN</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.signupButton} onPress = {goToSignUp} >
-          <Text style={styles.signupButton}>Registre-se</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
-  );
+          <TouchableOpacity style={styles.signupButton} onPress = {this.goToSignUp} >
+            <Text style={styles.signupButton}>Registre-se</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    )
+  }
 }
 
 styles = StyleSheet.create({
