55f78cfda0cdb019c98597867c05835ab7e0cf77
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2019-09-16 01:46:12 -0300

    fixed untracked files

--- ios/Pods/Target Support Files/Pods-itrade-tvOSTests/Pods-itrade-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_itrade_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_itrade_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-itrade/Pods-itrade-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_itrade : NSObject
-@end
-@implementation PodsDummy_Pods_itrade
-@end
