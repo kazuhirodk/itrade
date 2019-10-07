d6a7bf69189cfd7dd4265c798deb88a0860bc239
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2019-10-07 02:11:19 -0300

    add product list to home and create route to trade

---
+++ src/components/Product/ProductTrade.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet
+} from 'react-native';
+
+export default class ProductTrade extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    )
+  }
+}
+
+const state = {
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+    backgroundColor: '#FFFAFA',
+    alignContent: 'center'
+  }
+})
--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -6,16 +6,18 @@ import Home from '../Home/Home';
 import ProfileEdit from '../Profile/ProfileEdit';
 import ProductCreate from '../Product/ProductCreate';
 import ProductEdit from '../Product/ProductEdit';
+import ProductTrade from '../Product/ProductTrade';
 
 const Routes = () => (
   <Router>
     <Stack key="root">
-      <Scene key = "login" component = {Login} title = "Login" initial = {true} />
+      <Scene key = "login" component = {Login} title = "Login"  />
       <Scene key = "signup" component = {SignUp} title = "Cadastro" />
-      <Scene key = "home" component = {Home} title = "Home" />
+      <Scene key = "home" component = {Home} title = "Home" initial = {true} />
       <Scene key = "profileEdit" component = {ProfileEdit} title = "Editar Perfil" />
       <Scene key = "productCreate" component = {ProductCreate} title = "Cadastrar Produto" />
       <Scene key = "productEdit" component = {ProductEdit} title = "Editar Produto" />
+      <Scene key = "productTrade" component = {ProductTrade} title = "Troca de Produto" />
     </Stack>
   </Router>
 )
