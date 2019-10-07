06ebfe9770fe03c8ee98ef88bd5df7f08df1dd6e
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2019-10-07 03:19:20 -0300

    add trade screen

--- src/components/Product/ProductTrade.js
+++ src/components/Product/ProductTrade.js
@@ -1,26 +1,166 @@
-import React from 'react';
-import {
-  View,
-  StyleSheet
-} from 'react-native';
+import React, { Component } from 'react'
+import Swiper from 'react-native-deck-swiper'
+import { Image, Button, StyleSheet, Text, View } from 'react-native'
 
-export default class ProductTrade extends React.Component {
-  render() {
+export default class ProductTrade extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      cards: ['produto 1', 'produto 2'],
+      swipedAllCards: false,
+      swipeDirection: '',
+      cardIndex: 0
+    }
+  }
+
+  renderCard = (card, index) => {
+    return (
+      <View style={styles.card}>
+        <Image style={{width: 150, height: 150}} source={require('../../components/images/product1.png')}/>
+        <Text style={styles.text}>{card}</Text>
+      </View>
+    )
+  };
+
+  onSwiped = (type) => {
+    console.log(`on swiped ${type}`)
+  }
+
+  onSwipedAllCards = () => {
+    this.setState({
+      swipedAllCards: true
+    })
+  };
+
+  swipeLeft = () => {
+    this.swiper.swipeLeft()
+  };
+
+  render () {
     return (
       <View style={styles.container}>
+        <Swiper
+          ref={swiper => {
+            this.swiper = swiper
+          }}
+          onSwiped={() => this.onSwiped('general')}
+          onSwipedLeft={() => this.onSwiped('left')}
+          onSwipedRight={() => this.onSwiped('right')}
+          onSwipedTop={() => this.onSwiped('top')}
+          onSwipedBottom={() => this.onSwiped('bottom')}
+          onTapCard={this.swipeLeft}
+          cards={this.state.cards}
+          cardIndex={this.state.cardIndex}
+          cardVerticalMargin={80}
+          renderCard={this.renderCard}
+          onSwipedAll={this.onSwipedAllCards}
+          stackSize={3}
+          stackSeparation={15}
+          overlayLabels={{
+            bottom: {
+              title: 'BLEAH',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }
+              }
+            },
+            left: {
+              title: 'NOPE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-end',
+                  justifyContent: 'flex-start',
+                  marginTop: 30,
+                  marginLeft: -30
+                }
+              }
+            },
+            right: {
+              title: 'LIKE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-start',
+                  marginTop: 30,
+                  marginLeft: 30
+                }
+              }
+            },
+            top: {
+              title: 'SUPER LIKE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }
+              }
+            }
+          }}
+          animateOverlayLabelsOpacity
+          animateCardOpacity
+          swipeBackCard
+        >
+          <Button onPress={() => this.swiper.swipeBack()} title='Desfazer ação' />
+        </Swiper>
       </View>
     )
   }
 }
 
-const state = {
-}
-
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
     flex: 1,
-    backgroundColor: '#FFFAFA',
-    alignContent: 'center'
+    backgroundColor: '#F5FCFF'
+  },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#E8E8E8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    height: '50%'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 50,
+    backgroundColor: 'transparent'
+  },
+  done: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white',
+    backgroundColor: 'transparent'
   }
 })
