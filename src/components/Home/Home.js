4cec7227efb9cd22d6758437b085f2e5030a92ad
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2019-11-01 05:22:26 -0300

    remove getUniq function

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -31,20 +31,6 @@ const goToMatch = () => {
   Actions.matches()
 }
 
-const getUnique = (arr, comp) => {
-
-  const unique = arr
-    .map(e => e[comp])
-
-  // store the keys of the unique objects
-  .map((e, i, final) => final.indexOf(e) === i && i)
-
-  // eliminate the dead keys & store unique objects
-  .filter(e => arr[e]).map(e => arr[e]);
-
-  return unique;
-}
-
 export default class Home extends React.Component {
   key = ''
 
